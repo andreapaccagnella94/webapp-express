bb3e4ea579d0532b3921471959e8ee240ce9b113
andreapaccagnella94 <andreapacca94@live.it>
2025-11-09 22:27:51 +0100

    add update .env.example and implement movies API routes

---
+++ routes/movies.js
@@ -0,0 +1,54 @@
+const Router = require('express').Router();
+
+/* connection to database */
+const connection = require('../database/connection');
+
+Router.get('/', (req, res) => {
+
+    const sql = 'SELECT * FROM movies';
+    connection.query(sql, (err, results) => {
+        if (err) res.status(500).json({ error: err.message });
+
+        console.log(results);
+
+        res.json({ movies: results });
+    });
+})
+
+Router.get('/:id', (req, res) => {
+
+    const sql = 'SELECT * FROM movies WHERE id = ?';
+    const reviewsSql = 'SELECT * FROM reviews WHERE movie_id = ?';
+
+    const movieId = Number(req.params.id); // Number for safety injection
+    console.log(sql, movieId);
+
+    connection.query(sql, [movieId], (err, results) => {
+        if (err) res.status(500).json({ error: err.message });
+        console.log(results);
+
+        // verify if movie exists
+        if (results.length === 0) {
+            res.status(404).json({ message: 'Movie not found' })
+        }
+
+        // add second query for reviews
+        connection.query(reviewsSql, [movieId], (reviewsErr, reviewsResults) => {
+            if (reviewsErr) res.status(500).json({ error: reviewsErr.message })
+            console.log(reviewsResults);
+
+            // add const for create a object with reviews
+            const thisMovie = { ...results[0], reviews: reviewsResults };
+            res.json(thisMovie);
+
+        })
+
+    })
+
+})
+
+
+
+
+
+module.exports = Router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3000;
 /* connection to database */
 const connection = require('./database/connection');
 
+// import routes
+const moviesRoutes = require('./routes/movies');
+
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
 });
@@ -16,48 +19,5 @@ app.get('/', (req, res) => {
 });
 
 /* add more routes as needed for movies */
-app.get('/api/movies', (req, res) => {
+app.use('/api/movies', moviesRoutes);
 
-    const sql = 'SELECT * FROM movies';
-    connection.query(sql, (err, results) => {
-        if (err) res.status(500).json({ error: err.message });
-
-        console.log(results);
-
-        res.json({ movies: results });
-    });
-})
-
-app.get('/api/movies/:id', (req, res) => {
-
-    const sql = 'SELECT * FROM movies WHERE id = ?';
-    const reviewsSql = 'SELECT * FROM reviews WHERE movie_id = ?';
-
-    const movieId = Number(req.params.id); // Number for safety injection
-    console.log(sql, movieId);
-
-    connection.query(sql, [movieId], (err, results) => {
-        if (err) res.status(500).json({ error: err.message });
-        console.log(results);
-
-        // verify if movie exists
-        if (results.length === 0) {
-            res.status(404).json({ message: 'Movie not found' })
-        }
-
-        // add second query for reviews
-        connection.query(reviewsSql, [movieId], (reviewsErr, reviewsResults) => {
-            if (reviewsErr) res.status(500).json({ error: reviewsErr.message })
-            console.log(reviewsResults);
-
-            // add const for create a object with reviews
-            const thisMovie = { ...results[0], reviews: reviewsResults };
-            res.json(thisMovie);
-
-        })
-
-
-
-    })
-
-})
