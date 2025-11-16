30b8dd36099b93779d24bbc4391688a1b2b681a5
andreapaccagnella94 <andreapacca94@live.it>
2025-11-17 00:03:10 +0100

    refactor error handling in movie retrieval methods and update response structure; add new movie images and remove outdated ones

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -5,7 +5,7 @@ const connection = require('../database/connection');
 function index(req, res) {
     const sql = 'SELECT * FROM movies';
     connection.query(sql, (err, results) => {
-        if (err) res.status(500).json({ error: err.message });
+        if (err) return res.status(500).json({ error: err.message });
 
         console.log(results);
 
@@ -19,21 +19,21 @@ function show(req, res) {
     const reviewsSql = 'SELECT * FROM reviews WHERE movie_id = ?';
 
     const movieId = Number(req.params.id); // Number for safety injection
-    console.log(sql, movieId);
+    // console.log(sql, movieId);
 
     connection.query(sql, [movieId], (err, results) => {
-        if (err) res.status(500).json({ error: err.message });
-        console.log(results);
+        if (err) return res.status(500).json({ error: err.message });
+        // console.log(results);
 
         // verify if movie exists
         if (results.length === 0) {
-            res.status(404).json({ message: 'Movie not found' })
+            return res.status(404).json({ message: 'Movie not found' })
         }
 
         // add second query for reviews
         connection.query(reviewsSql, [movieId], (reviewsErr, reviewsResults) => {
-            if (reviewsErr) res.status(500).json({ error: reviewsErr.message })
-            console.log(reviewsResults);
+            if (reviewsErr) return res.status(500).json({ error: reviewsErr.message })
+            // console.log(reviewsResults);
 
             // add const for create a object with reviews
             const thisMovie = { ...results[0], reviews: reviewsResults };
